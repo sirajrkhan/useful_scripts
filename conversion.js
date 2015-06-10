7ced1a2cfdfe3f99b8da5afc947070d3c81841ec
sirajrkhan <sirajrkhan@live.in>
2015-06-10 12:10:57 +0000

    Celsius to Fah and vise-versa using Node CLI

---
+++ conversion.js
@@ -0,0 +1,23 @@
+//console.log('Bismillah');
+
+(function(){
+
+    //accessing the command line parameters
+    var wNum = process.argv[2];
+    var wUnit = process.argv[3];
+    
+    // the real action happens here...........
+    var con = function(u,n){
+        if (u === "F" || u === "f"){
+            console.log ((n-32) * (5/9) + 'C');
+        } else if (u === "C" || u === "c"){
+            console.log ((n * (9/5)) + 32 +'F');
+        } else{
+            console.log('invalid input');
+        }
+    }
+    
+    //executing
+    con(wUnit,wNum);
+    
+})();
