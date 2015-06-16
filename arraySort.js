7b792771f5e8b79df6da74ccf2c7440f5fad266a
sirajrkhan <sirajrkhan@live.in>
2015-06-16 14:19:37 +0000

    completed the functionality with command line param-- few corner cases need to be fixed

--- arraySort.js
+++ arraySort.js
@@ -3,25 +3,8 @@
 (function(){
 
     //Dummy data for checking
-    var strArray = "['Apple','Orange','Mango','Banana','Guava']";
+    //var strArray = "['Apple','Orange','Mango','Banana','Guava']";
     //var numArray = [21,1,101,40,301];
-
-    //accessing the command line parameter
-    //Expectation: "node arraySort.js [1,3,6,2]"
-    var param = process.argv[2];
-    
-    //check for String for Numeric Array and sort accordingly
-    var iSort = function(a){
-        if (!param || false === (param instanceof Array)){
-            console.log ('invalid or no parameters');
-        }else if (chkNum(a) === false){
-            a.sort(); 
-            console.log(a.toString());
-        } else{
-            a.sort(function(num1,num2){return num1-num2});
-            console.log(a.toString());
-        } 
-    }
 
     //Check for pure Numeric Array
     var chkNum = function (ar){
@@ -33,21 +16,32 @@
                 x++;
             }
         }
-    }
+    }    
     
-    //makeshift solution for converting the command-line inputs to array
-    //takes input as string and returns array
+    //makeshift solution for converting the command-line inputs to Array
     var makeArr = function (str){
-        
-        str = str.substring(1,str.length-1);
-        str = str.split(",");
-        console.log( str instanceof Array);
-        console.log(str.toString())
+        if (str && !(str instanceof Array)){
+            str = str.substring(1,str.length-1);
+            str = str.split(",");
+            return str;
+        }
     }
+
+    //accessing the command line parameter
+    //Expectation: "node arraySort.js [1,3,6,2]"
+    var param = makeArr(process.argv[2]);
     
-    makeArr(strArray);
+    //check for String for Numeric Array and sort accordingly
+    var iSort = function(a){
+        if (!param || false === (param instanceof Array)){
+            console.log ('invalid or no parameters');
+        }else if (chkNum(a) === false){
+            a.sort(); 
+        } else{
+            a.sort(function(num1,num2){return num1-num2});
+        } 
+    }
 
     //Command line would work like this: node arraySort.js[space][array]
-    //iSort(param);
-
+    iSort(param);
 })();
